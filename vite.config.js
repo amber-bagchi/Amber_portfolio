dead947f2801d44d4d719376e64f0218c4649780
Amber Bagchi <amberbagchi2604@gmail.com>
2024-09-28 19:32:42 +0530

    build change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/*.glb'],  // Include .glb assets (for 3D models, etc.)
+  
+  // Explicitly set the output directory to 'dist'
+  build: {
+    outDir: 'dist',  // Ensure built files are placed in the 'dist' directory
+    chunkSizeWarningLimit: 2000,  // Optional: increase chunk size limit if needed
+  }
 })
